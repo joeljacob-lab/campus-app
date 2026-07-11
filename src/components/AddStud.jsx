58ecfd57f2c1852c831e5a7461eb9ceb82dfa334
Joel <joelj712@gmail.com>
2026-07-11 15:19:44 +0530

    small changes

--- src/components/AddStud.jsx
+++ src/components/AddStud.jsx
@@ -84,7 +84,7 @@ const AddStud = () => {
                         
                         <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
 
-                                <button className="btn btn-primary">Add HouseBoat</button>
+                                <button className="btn btn-primary">Add Student</button>
 
                         </div>
 
--- src/components/ViewAll.jsx
+++ src/components/ViewAll.jsx
@@ -22,7 +22,7 @@ const ViewAll = () => {
 
     return (
         <div>
-            <NavigationBar />
+            
             <div className="container">
                 <div className="row">
                     <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
